039150e151fb74aeaa61111d10b6cc3ba118f671
Stas Melnychenko <shakenbeer@gmail.com>
2018-05-29 08:49:14 +0300

    starting ui

---
+++ app/src/main/java/com/shakenbeer/customviewnavigation/FrameLayoutHost.kt
@@ -0,0 +1,21 @@
+package com.shakenbeer.customviewnavigation
+
+import android.content.Context
+import android.util.AttributeSet
+import android.widget.FrameLayout
+import androidx.navigation.NavController
+import androidx.navigation.NavHost
+
+class FrameLayoutHost @JvmOverloads constructor(
+        context: Context, attrs: AttributeSet? = null, defStyleAttr: Int = 0
+) : FrameLayout(context, attrs, defStyleAttr),  NavHost {
+
+    private val mNavController = NavController(context)
+
+
+
+    override fun getNavController(): NavController {
+        return mNavController
+    }
+}
+
--- app/src/main/java/com/shakenbeer/customviewnavigation/MainActivity.kt
+++ app/src/main/java/com/shakenbeer/customviewnavigation/MainActivity.kt
@@ -2,11 +2,16 @@ package com.shakenbeer.customviewnavigation
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import androidx.navigation.Navigation
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        val customViewNavigator = CustomViewNavigator(container)
+        val navController = Navigation.findNavController(this, R.id.container)
+        navController.navigatorProvider.addNavigator(customViewNavigator)
     }
 }
